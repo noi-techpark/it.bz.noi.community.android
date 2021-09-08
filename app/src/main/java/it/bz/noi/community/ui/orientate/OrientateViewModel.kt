1a475efff199ad269d1a6152f42413b793134d87
Stefano Zanella <stefano.zanella@dimension.it>
2021-09-08 12:46:07 +0200

    changed placeholders

--- app/src/main/java/it/bz/noi/community/ui/orientate/OrientateViewModel.kt
+++ app/src/main/java/it/bz/noi/community/ui/orientate/OrientateViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class OrientateViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is Orientate Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/it/bz/noi/community/ui/today/TodayViewModel.kt
+++ app/src/main/java/it/bz/noi/community/ui/today/TodayViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class TodayViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is Today Fragment"
     }
     val text: LiveData<String> = _text
 }
