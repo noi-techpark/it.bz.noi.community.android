b3cb371c8b0c53de35f27f73aa5490786120bfad
Stefano Zanella <stefano.zanella@dimension.it>
2021-10-01 16:28:10 +0100

    added splash screen

--- app/src/main/java/it/bz/noi/community/SplashScreenActivity.kt
+++ app/src/main/java/it/bz/noi/community/SplashScreenActivity.kt
@@ -1,16 +1,46 @@
 package it.bz.noi.community
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.MediaController
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
+import it.bz.noi.community.databinding.ActivityMainBinding
+import it.bz.noi.community.databinding.ActivitySplashBinding
 
 /**
  * Activity used only for displaying the Splash/Launch Screen
  */
 class SplashScreenActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
-    }
+
+	private lateinit var binding: ActivitySplashBinding
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binding = ActivitySplashBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+		try {
+
+			val video =
+				Uri.parse("android.resource://" + packageName + "/" + R.raw.noi_splash_screen_video)
+			binding.vwSplash.apply {
+				setVideoURI(video)
+				setOnCompletionListener {
+					jump()
+				}
+				requestFocus()
+				start()
+			}
+		} catch (ex: Exception) {
+			jump()
+		}
+	}
+
+	private fun jump() {
+		if (isFinishing) return
+		startActivity(Intent(this, MainActivity::class.java))
+		finish()
+	}
 }
