4f86c33dce6c7c1a8abdfd8fcb8befae6b15d4b3
Stefano Zanella <stezan97@gmail.com>
2021-10-04 08:48:40 +0200

    Added splash screen (#35)
    Added the splash screen that visualise the video of the NOI community. When the video finish, the user is moved to the main screen.
    
    Implements #2

--- app/src/main/java/it/bz/noi/community/SplashScreenActivity.kt
+++ app/src/main/java/it/bz/noi/community/SplashScreenActivity.kt
@@ -1,16 +1,42 @@
 package it.bz.noi.community
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import it.bz.noi.community.databinding.ActivitySplashBinding
 
 /**
  * Activity used only for displaying the Splash/Launch Screen
  */
 class SplashScreenActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
-    }
+
+	private lateinit var binding: ActivitySplashBinding
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binding = ActivitySplashBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+		try {
+
+			val video =
+				Uri.parse("android.resource://" + packageName + "/" + R.raw.noi_splash_screen_video)
+			binding.vwSplash.apply {
+				setVideoURI(video)
+				setOnCompletionListener {
+					goToMainActivity()
+				}
+				requestFocus()
+				start()
+			}
+		} catch (ex: Exception) {
+			goToMainActivity()
+		}
+	}
+
+	private fun goToMainActivity() {
+		if (isFinishing) return
+		startActivity(Intent(this, MainActivity::class.java))
+		finish()
+	}
 }
