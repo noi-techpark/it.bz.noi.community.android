028ea5ffee5402f47b4284d8bcfcfe90a6c60e2c
Chiara Pellegrini <chiara.pellegrini@dimension.it>
2021-10-08 16:02:19 +0200

    Buttons replaced with MaterialButtons

--- app/src/main/java/it/bz/noi/community/ui/orientate/OrientateFragment.kt
+++ app/src/main/java/it/bz/noi/community/ui/orientate/OrientateFragment.kt
@@ -5,10 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton
 import it.bz.noi.community.R
 import it.bz.noi.community.ui.WebViewFragmentDirections
 
@@ -25,7 +24,7 @@ class OrientateFragment : Fragment() {
         webView.settings.javaScriptEnabled = true
         webView.loadUrl(resources.getString(R.string.url_map))
 
-        val btn = root.findViewById<Button>(R.id.roomBookingBtn)
+        val btn = root.findViewById<MaterialButton>(R.id.roomBookingBtn)
         btn.setOnClickListener {
             val action = WebViewFragmentDirections.actionGlobalWebViewFragment().apply {
 				title = resources.getString(R.string.room_booking)
