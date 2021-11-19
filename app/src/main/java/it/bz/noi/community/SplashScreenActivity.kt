7193ab544a5280d20f36d4bf96e698766adf6689
Chiara Pellegrini <chiara.pellegrini@dimension.it>
2021-11-19 16:49:16 +0100

    shared preferences to manage splash screen visualization

--- app/src/main/java/it/bz/noi/community/SplashScreenActivity.kt
+++ app/src/main/java/it/bz/noi/community/SplashScreenActivity.kt
@@ -1,9 +1,12 @@
 package it.bz.noi.community
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import it.bz.noi.community.databinding.ActivitySplashBinding
 
 /**
@@ -11,10 +14,23 @@ import it.bz.noi.community.databinding.ActivitySplashBinding
  */
 class SplashScreenActivity : AppCompatActivity() {
 
+	private lateinit var sharedPreferences: SharedPreferences
 	private lateinit var binding: ActivitySplashBinding
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+
+		sharedPreferences = getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE)
+
+		/**
+		 * The splash screen has to be shown only the very first time the user opens the app
+		 * (see https://github.com/noi-techpark/it.bz.noi.community.android/issues/74)
+		 */
+		if (sharedPreferences.getBoolean(SKIP_PARAM, false)) {
+			goToMainActivity()
+			return
+		}
+
 		binding = ActivitySplashBinding.inflate(layoutInflater)
 		setContentView(binding.root)
 		try {
@@ -24,6 +40,9 @@ class SplashScreenActivity : AppCompatActivity() {
 			binding.vwSplash.apply {
 				setVideoURI(video)
 				setOnCompletionListener {
+					sharedPreferences.edit {
+						putBoolean(SKIP_PARAM, true)
+					}
 					goToMainActivity()
 				}
 				requestFocus()
@@ -32,11 +51,18 @@ class SplashScreenActivity : AppCompatActivity() {
 		} catch (ex: Exception) {
 			goToMainActivity()
 		}
+
+
 	}
 
 	private fun goToMainActivity() {
 		if (isFinishing) return
 		startActivity(Intent(this, MainActivity::class.java))
 		finish()
+	}
+
+	companion object {
+		private const val SHARED_PREFS_NAME = "noi_shared_prefs"
+		private const val SKIP_PARAM = "skip_splash_screen"
 	}
 }
