f554563cc33b0f6811b3469d26c7c573875212bd
Chiara Pellegrini <chiara.pellegrini@dimension.it>
2022-04-08 16:29:51 +0200

    WIP: news detail

--- app/src/main/java/it/bz/noi/community/ui/newsDetails/NewsDetailsFragment.kt
+++ app/src/main/java/it/bz/noi/community/ui/newsDetails/NewsDetailsFragment.kt
@@ -1,16 +1,38 @@
 package it.bz.noi.community.ui.newsDetails
 
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import it.bz.noi.community.data.api.ApiHelper
+import it.bz.noi.community.data.api.RetrofitBuilder
+import it.bz.noi.community.data.models.NewsImage
+import it.bz.noi.community.data.models.getContactInfo
+import it.bz.noi.community.data.models.getDetail
 import it.bz.noi.community.databinding.FragmentNewsDetailsBinding
+import it.bz.noi.community.databinding.VhHorizontalImageBinding
+import it.bz.noi.community.utils.Status
+import kotlinx.coroutines.Dispatchers
+import java.text.DateFormat
 
 class NewsDetailsFragment: Fragment() {
 
 	private var _binding: FragmentNewsDetailsBinding? = null
 	private val binding get() = _binding!!
+
+	private val viewModel: NewsDetailViewModel by viewModels(factoryProducer = {
+		NewsDetailViewModelFactory(apiHelper = ApiHelper(RetrofitBuilder.apiService), this@NewsDetailsFragment)
+	})
+
+	private val df = DateFormat.getDateInstance(DateFormat.SHORT) // FIXME chiedere tipo di formattazione
 
 	override fun onCreateView(
 		inflater: LayoutInflater,
@@ -26,4 +48,80 @@ class NewsDetailsFragment: Fragment() {
 		_binding = null
 	}
 
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		viewModel.news.asLiveData(Dispatchers.Main).observe(viewLifecycleOwner) {
+			when (it.status) {
+				Status.SUCCESS -> {
+					val news = it.data!!
+					news.getDetail()?.let { detail ->
+						binding.title.text = detail.title
+						binding.shortText.text = detail.abstract
+						binding.longText.text = Html.fromHtml(detail.text, Html.FROM_HTML_MODE_COMPACT)
+					}
+					news.getContactInfo()?.let { contactInfo ->
+						binding.publisher.text = contactInfo.publisher
+						Glide
+							.with(binding.root.context)
+							.load(contactInfo.logo)
+							.centerCrop()
+							.into(binding.logo)
+
+					}
+					binding.date.text = df.format(news.date)
+
+					if (news.images != null && news.images.isNotEmpty()) {
+						binding.images.isVisible = true
+						binding.images.layoutManager = LinearLayoutManager(binding.root.context, LinearLayoutManager.HORIZONTAL, false)
+						binding.images.adapter = NewsImagesAdapter(news.images)
+					} else {
+						binding.images.isVisible = false
+					}
+				}
+				Status.ERROR -> {
+
+				}
+				Status.LOADING -> {
+
+				}
+			}
+		}
+
+	}
+
 }
+
+/**
+ * Adapter used to populate the image gallery of news detail
+ */
+class NewsImagesAdapter(private val images: List<NewsImage>) : RecyclerView.Adapter<NewsImagesAdapter.NewsImageViewHolder>() {
+
+	/**
+	 * View holder of a single picture
+	 */
+	inner class NewsImageViewHolder(private val binding: VhHorizontalImageBinding) : RecyclerView.ViewHolder(binding.root) {
+
+		fun bind(image: NewsImage) {
+			Glide
+				.with(binding.root.context)
+				.load(image.url)
+				.centerCrop()
+				.into(binding.restImage)
+		}
+	}
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsImageViewHolder {
+		return NewsImageViewHolder(VhHorizontalImageBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+	}
+
+	override fun onBindViewHolder(holder: NewsImageViewHolder, position: Int) {
+		holder.bind(images[position])
+	}
+
+	override fun getItemCount(): Int {
+		return images.size
+	}
+}
+
+
