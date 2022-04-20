4fdba923d29c3db54eddecdc8ceb21fd72d28f7e
Chiara Pellegrini <chiara.pellegrini@dimension.it>
2022-04-20 17:36:33 +0200

    WIP: changed gradient on onboarding background images

--- app/src/main/java/it/bz/noi/community/ui/onboarding/BaseOnboardingFragment.kt
+++ app/src/main/java/it/bz/noi/community/ui/onboarding/BaseOnboardingFragment.kt
@@ -1,16 +1,56 @@
 package it.bz.noi.community.ui.onboarding
 
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.graphics.drawable.PaintDrawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.RectShape
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.ColorUtils
 import androidx.fragment.app.Fragment
+import it.bz.noi.community.R
 import it.bz.noi.community.databinding.FragmentOnboardingBinding
 
 open class BaseOnboardingFragment : Fragment() {
 
 	private var _binding: FragmentOnboardingBinding? = null
 	internal val binding get() = _binding!!
+
+	private val gradientDrawable = PaintDrawable().apply {
+		val sf: ShapeDrawable.ShaderFactory = object : ShapeDrawable.ShaderFactory() {
+			override fun resize(width: Int, height: Int): Shader {
+				return LinearGradient(
+					0f, height.toFloat(), 0f, 0f,
+					listOf(
+						ColorUtils.setAlphaComponent(
+							requireContext().getColor(R.color.background_color),
+							255
+						), // alpha = 100%
+						ColorUtils.setAlphaComponent(
+							requireContext().getColor(R.color.background_color),
+							222
+						), // alpha = 87%
+						ColorUtils.setAlphaComponent(
+							requireContext().getColor(R.color.background_color),
+							153
+						), // alpha = 60%
+						ColorUtils.setAlphaComponent(
+							requireContext().getColor(R.color.background_color),
+							0
+						), // alpha = 0%
+					).toIntArray(),
+					listOf(0f, 0.38f, 0.72f, 1f).toFloatArray(),
+					Shader.TileMode.CLAMP
+				)
+			}
+		}
+
+		shape = RectShape()
+		shaderFactory = sf
+	}
 
 	override fun onCreateView(
 		inflater: LayoutInflater,
@@ -24,6 +64,11 @@ open class BaseOnboardingFragment : Fragment() {
 	override fun onDestroyView() {
 		super.onDestroyView()
 		_binding = null
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		binding.image.foreground = gradientDrawable
 	}
 
 }
