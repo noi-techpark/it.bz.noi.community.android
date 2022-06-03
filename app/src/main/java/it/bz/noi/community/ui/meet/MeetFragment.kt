85d5c208d5689278ade37b4ab7df7881d00fae64
Chiara Pellegrini <chiara.pellegrini@dimension.it>
2022-06-03 16:55:53 +0200

    WIP: managed empty state of tab Meet

--- app/src/main/java/it/bz/noi/community/ui/meet/MeetFragment.kt
+++ app/src/main/java/it/bz/noi/community/ui/meet/MeetFragment.kt
@@ -1,5 +1,6 @@
 package it.bz.noi.community.ui.meet
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,9 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
-import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.lifecycleScope
@@ -24,6 +23,7 @@ import it.bz.noi.community.data.api.RetrofitBuilder
 import it.bz.noi.community.data.models.Contact
 import it.bz.noi.community.databinding.FragmentMeetBinding
 import it.bz.noi.community.databinding.VhContactBinding
+import it.bz.noi.community.databinding.VhEmptyBinding
 import it.bz.noi.community.utils.Status
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
@@ -81,13 +81,13 @@ class MeetFragment : Fragment() {
 				Status.SUCCESS -> {
 					val contacts = res.data!!
 					Log.d(TAG, "Caricati ${contacts.size} contatti")
-					contactsAdapter.items = contacts
+					contactsAdapter.state = ContactsAdapter.AdapterState.Ok(contacts)
 					binding.swipeRefreshContacts.isRefreshing = false
 				}
 				Status.ERROR -> {
-					contactsAdapter.items = emptyList()
 					Log.d(TAG, "Caricamento contatti KO")
 					binding.swipeRefreshContacts.isRefreshing = false
+					// FIXME gestire uno state nell'adapter??
 					Toast.makeText(requireContext(), res.message, Toast.LENGTH_LONG).show()
 				}
 				Status.LOADING -> {
@@ -98,11 +98,10 @@ class MeetFragment : Fragment() {
 		}
 
 		binding.searchFieldEditText.addTextChangedListener { text ->
-			Log.d(TAG, "searchFieldEditText value: $text")
 			viewModel.updateSearchParam(text)
 		}
 
-		// FIXME il testo dovrebbe venir ripristinato in automatico al cambio di tab
+		// FIXME provare con multiple back stacks
 		viewLifecycleOwner.lifecycleScope.launch {
 			repeatOnLifecycle(Lifecycle.State.STARTED) {
 				viewModel.searchParamFlow.collectLatest {
@@ -126,26 +125,89 @@ interface ContactDetailListener {
 	fun openContactDetail(contact: Contact)
 }
 
-class ContactsAdapter(private val detailListener: ContactDetailListener) : RecyclerView.Adapter<ContactVH>() {
+class ContactsAdapter(private val detailListener: ContactDetailListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-	var items = emptyList<Contact>()
+	companion object {
+		const val TAG = "ContactsAdapter"
+		const val CONTACT = 0
+		const val LOADING = 1
+		const val EMPTY = 3
+	}
+
+
+	sealed class AdapterState {
+		data class Ok(val contacts: List<Contact> = emptyList()) : AdapterState()
+		object Loading : AdapterState()
+	}
+
+	sealed class Item(val viewType: Int) {
+		object Loading: Item(LOADING)
+		object Empty: Item(EMPTY)
+		data class Contact(val contact: it.bz.noi.community.data.models.Contact) : Item(CONTACT)
+	}
+
+	var state: AdapterState = AdapterState.Loading
+		set(value) {
+			if (field != value) {
+				field = value
+				items = value.toItems()
+			}
+		}
+
+	private var items: List<Item> = listOf(Item.Loading)
+		@SuppressLint("NotifyDataSetChanged")
 		set(value) {
 			field = value
 			notifyDataSetChanged()
 		}
 
-	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactVH {
-		return ContactVH(VhContactBinding.inflate(LayoutInflater.from(parent.context), parent, false), detailListener)
+	private fun AdapterState.toItems(): List<Item> {
+		return when (this) {
+			is AdapterState.Ok -> when (contacts.size) {
+				0 -> listOf(Item.Empty)
+				else -> contacts.map { c ->
+					Item.Contact(c)
+				}
+			}
+			is AdapterState.Loading -> listOf(Item.Loading)
+		}
 	}
 
-	override fun onBindViewHolder(holder: ContactVH, position: Int) {
-		items.get(position)?.let {
-			holder.bind(it)
+	override fun getItemViewType(position: Int): Int = items[position].viewType
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+		return when (viewType) {
+			CONTACT -> ContactVH(VhContactBinding.inflate(LayoutInflater.from(parent.context), parent, false), detailListener)
+			LOADING -> EmptyViewHolder(
+				VhEmptyBinding.inflate(LayoutInflater.from(parent.context), parent, false).apply {
+					title.isVisible = false
+					subtitle.isVisible = false
+				}
+			)
+			EMPTY -> EmptyViewHolder(
+				VhEmptyBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+			)
+			else -> throw UnkownViewTypeException(viewType)
+		}
+	}
+
+
+	override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+		when (holder.itemViewType) {
+			CONTACT -> {
+				(items[position] as Item.Contact).let { txItem ->
+					(holder as ContactVH).bind(txItem.contact)
+				}
+			}
 		}
 	}
 
 	override fun getItemCount(): Int = items.size
 }
+
+class UnkownViewTypeException(id: Int) : Exception("Unkown view type $id")
+
+class EmptyViewHolder(binding: VhEmptyBinding) : RecyclerView.ViewHolder(binding.root)
 
 class ContactVH(private val binding: VhContactBinding, detailListener: ContactDetailListener) : RecyclerView.ViewHolder(binding.root) {
 
