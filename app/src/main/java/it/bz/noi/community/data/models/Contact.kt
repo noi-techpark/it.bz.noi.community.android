0b757ca19416d43ee58e3924d2cb6094e9a3cc6a
Chiara Pellegrini <chiara.pellegrini@dimension.it>
2022-06-09 13:05:23 +0200

    Added @keep annotation on Parcelable class used as navigation arguments

--- app/src/main/java/it/bz/noi/community/data/models/Contact.kt
+++ app/src/main/java/it/bz/noi/community/data/models/Contact.kt
@@ -1,9 +1,11 @@
 package it.bz.noi.community.data.models
 
 import android.os.Parcelable
+import androidx.annotation.Keep
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
+@Keep
 @Parcelize
 data class Contact(
 	@SerializedName("contactid")
